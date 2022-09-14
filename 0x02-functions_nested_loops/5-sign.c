72e2b583530a6d159a9d2683ba90f77aeac3c2f6
Olajumoke <Olajumoke>
2022-09-13 17:20:38 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  * and prints the sign (+ / -)
  * @n: The integer to check
  *
- * Return: 1 if n is greater than zero, and prints 
+ * Return: 1 if n is greater than zero, and prints
  * returns 0 if n is zero, and prints 0
  * returns -1 if n is less than zero, and prints -
  */
